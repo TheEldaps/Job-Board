45d65c027a483b864c5aef4bf3873ae6dd2c441c
TheEldaps <adejames97@gmail.com>
2025-06-14 02:51:50 +0100

    Add new App logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import NavImg from "../assets/TechNest-01.png";
+import NavImg from "../assets/TechNestLogo.png";
 
 export default function Nav({ setShowNav, showNav }) {
   const [addBg, setAddBg] = useState("");
